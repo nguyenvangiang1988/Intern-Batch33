0167ffb3d1e1ba558f9e75a59d54e80cdeb9789a
lqdoan <48527422+lqdoan@users.noreply.github.com>
2021-06-21 15:48:53 +0700

    Add files via upload

---
+++ lqdoan_ls_cat_command/main.c
@@ -0,0 +1,23 @@
+#include "header.h"
+int main() {
+  int running = 1, argv;
+  char *args[MAXLEN / 2 + 1];
+  while (running) {
+    argv = 0;
+    printf("\033[0;32m");  // set color
+    printf("cmd>");
+    fflush(stdout);
+    printf("\033[0m");  // reset color
+    running = readCommand(args, &argv);
+
+    int status;
+    if (!running) return 0;
+    /*
+    for (int i = 0; i < argv; ++i)
+            printf("input: %s \n", args[i]);
+    */
+    if (argv > 0) status = processCommand(args, &argv);
+    if (status == 1) printf("Command error.\n");
+  }
+  return 0;
+}
